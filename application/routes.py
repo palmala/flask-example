1a8119321af510404e6f1930ecf40d752e8f0153
palmala <palmala@users.noreply.github.com>
2021-10-17 20:13:51 +0200

    Adding navigation bar.

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,23 @@
 from application import app
 from flask import render_template
 
+
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+
+@app.route("/courses")
+def courses():
+    return render_template("not_implemented_yet.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("not_implemented_yet.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("not_implemented_yet.html")
